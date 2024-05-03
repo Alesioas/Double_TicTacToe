8b4a5d0dd7d11fbc8f914cd87112a673f85ac70e
Alesioas <lobovskileks@gmail.com>
2024-05-04 00:16:06 +0500

    Окно приветствия с кнопками ПОЕХАЛИ и ВЫХОД + добавление окна с кнопками СТАРТ, НАСТРОЙКИ, ИНФОРМАЦИЯ

--- app/src/main/java/com/belov/double_tictactoe/MainActivity.java
+++ app/src/main/java/com/belov/double_tictactoe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.belov.double_tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent switcher = new Intent(MainActivity.this,GameStart.class);
+                    MainActivity.this.startActivity(switcher);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -25,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void clickexit(View v){
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 }
