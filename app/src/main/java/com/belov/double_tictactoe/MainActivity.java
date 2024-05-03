40a5201e7b912ff5be01c78dbdd947edbb1ba232
Alesioas <lobovskileks@gmail.com>
2024-05-03 15:08:43 +0500

    Окно приветствия

--- app/src/main/java/com/belov/double_tictactoe/MainActivity.java
+++ app/src/main/java/com/belov/double_tictactoe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.belov.double_tictactoe;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
